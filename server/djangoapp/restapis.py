2f3d5b4722bb3215367dadbef8fb86099c2786a4
dong zhang <windyheratz5201314@gmail.com>
2025-12-02 20:16:45 -0800

    car model and dealer model add and update

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,22 +1,46 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+backend_url = os.getenv("backend_url", default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    "sentiment_analyzer_url", default="http://localhost:5050/"
+)
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+def get_request(endpoint, **kwargs):
+    # Add code for get requests to back end
+    request_url = backend_url + endpoint
+    print(f"GET from {request_url}")
+    try:
+        response = requests.get(request_url, params=kwargs)
+        return response.json()
+    except:
+        print("Network exception occurred")
+        return None
 
-# def post_review(data_dict):
-# Add code for posting review
+
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url + "analyze/" + text
+    # Add code for retrieving sentiments
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
+        return {"sentiment": "neutral"}
+
+
+def post_review(data_dict):
+    # Add code for posting review
+    request_url = backend_url + "/insert_review"
+    try:
+        response = requests.post(request_url, json=data_dict)
+        return response.json()
+    except:
+        print("Network exception occurred")
+        return None
