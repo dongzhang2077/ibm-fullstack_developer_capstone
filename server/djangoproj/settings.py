9cfcb2f70058144490754f76f7aeaca28f38f8cb
dong zhang <windyheratz5201314@gmail.com>
2025-12-02 22:54:03 -0800

    Fix database permissions for non-root container

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = "djangoproj.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+import os
+
+# Use /app/data for database in production (writable directory)
+DB_PATH = os.getenv('DB_PATH', str(BASE_DIR / "db.sqlite3"))
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": DB_PATH,
     }
 }
 
